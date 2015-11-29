baf65df7dbc69d940eb875a8bddda7af9d550470
darkeox <darkeox@PHARCHXTI>
2015-11-29 02:41:43 +0100

    Drills. Couldn't one line.

--- ex17.py
+++ ex17.py
@@ -1,25 +1,29 @@
+# from sys import argv
+# from os.path import exists
+#
+# script, from_file, to_file = argv
+#
+# print("Copying from {!s} to {!s}".format(from_file, to_file))
+#
+# # direct one liner
+# with open(from_file) as in_file : in_data = in_file.read()
+#
+# print("The input file is {:d} bytes long.".format(len(in_data)))
+#
+# print("Does the output file exists? {!r}".format(exists(to_file)))
+# print("Ready, hit RETURN to continue, CTRL-C to abort.")
+# input()
+#
+# # see context manager
+# # close resources when the exiting context: avoid leaving file object open for example
+# # when said fo where never given a representation through a variable.
+# with open(to_file, 'w') as out_file : out_file.write(in_data)
+#
+# print("Alright, all done.")
+
+# One line?
 from sys import argv
-from os.path import exists
 
 script, from_file, to_file = argv
 
-print("Copying from {!s} to {!s}".format(from_file, to_file))
-
-# direct one liner
-with open(from_file) as in_file : in_data = in_file.read()
-
-print("The input file is {:d} bytes long.".format(len(in_data)))
-
-print("Does the output file exists? {!r}".format(exists(to_file)))
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
-input()
-
-# see context manager
-# close resources when the exiting context: avoid leaving file object open for example
-# when said fo where never given a representation through a variable.
-with open(to_file, 'w') as out_file : out_file.write(in_data)
-
-print("Alright, all done.")
-
-
-
+open(argv[2], 'w').write(open(argv[1]).read())
