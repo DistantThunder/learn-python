129cf266f775e0e5ac0c1e849de55db14b38bf53
darkeox <darkeox@PHARCHXTI>
2016-02-23 02:37:02 +0100

    Test final

--- coding_test.py
+++ coding_test.py
@@ -1,7 +1,9 @@
 for i in range(2, 51):
-    if not(i % 3):
-        print("Three")
-    if not(i % 5):
-        print("Five")
+    if not(i % 3) and not(i % 5):
+        print("Three + Five")
+        elif not(i % 3):
+            print("Three")
+        elif not(i % 5):
+            print("Five")
     else:
         print(i)
