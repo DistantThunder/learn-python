196f03ed22663687ae0289edbae537dad2c5c9f9
darkeox <darkeox@PHARCHXTI>
2016-04-04 21:26:55 +0200

    Corrections, polish.

--- ex26.py
+++ ex26.py
@@ -61,7 +61,7 @@ print("--------------")
 print(poem)
 print("--------------")
 
-five = 10 - 2 + 3 - 5
+five = 10 - 2 + 3 - 6
 print("This should be five: ", five)
 
 def secret_formula(started):
@@ -77,10 +77,10 @@ beans, jars, crates = secret_formula(start_point)
 print("With a starting point of: %d" % start_point)
 print("We'd have {:d} jeans, {:.0f} jars, and {:.0f} crates.".format(beans, jars, crates))
 
-start_point = start_point / 10
+start_point /= 10
 
 print("We can also do that this way:")
-print("We'd have {:.0f} beans, {:.0f} jars, and {:.0f} crates.".format(secret_formula(start_point)))
+print("We'd have {0[0]:.0f} beans, {0[1]:.0f} jars, and {0[2]:.0f} crates.".format(secret_formula(start_point)))
 
 
 sentence = "All good\tthings come to those who weight."
