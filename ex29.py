3bae8b5f0e8009dd8aff33fe07fb7b4349b7f86b
darkeox <darkeox@PHARCHXTI>
2016-05-05 15:57:18 +0200

    Student drills.

--- ex29.py
+++ ex29.py
@@ -28,3 +28,10 @@ if people <= dogs:
 if people == dogs:
     print("People are dogs.")
 
+# Study Drills:
+# 1. "if" evaluates the code under it only if the expression in front of it is True.
+# 2. The code needs to be indented for the Python interpreter to understand that it is part of the "if" block.
+# 3. If the code isn't indented by four spaces, it always gets evaluated by the interpreter,
+# regardless of the 'if' statement.
+# 4. The python interpreter throws an error on stdout about undefined "names" (variables).
+
