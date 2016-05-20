0f89bf359ff866d4a9b293c56bb6393a23686a2a
darkeox <darkeox@PHARCHXTI>
2016-05-20 19:59:29 +0200

    Drill #1.

--- ex33.py
+++ ex33.py
@@ -1,15 +1,21 @@
-i = 0
 numbers = []
 
-while i < 6:
-    print("At the top i is {}".format(i))
-    numbers.append(i)
+# while i < 6:
+#     print("At the top i is {}".format(i))
+#     numbers.append(i)
+#
+#     i = i + 1
+#     print("Numbers now: ", numbers)
+#     print("At the bottom i is {}\n{}".format(i, '-'))
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print("At the bottom i is {}\n{}".format(i, '-'))
 
+def count_numbers(count):
+    count += 1
+    for i in range(0, count):
+        numbers.append(i)
+    return 0
 
+count_numbers(int(input("Enter the number you want to count to: ")))
 print("The numbers: ")
 
 for num in numbers:
