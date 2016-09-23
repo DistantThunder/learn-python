1096e3785f498669272f687c24fecce7cd2f2d12
darkeox <darkeox@PHARCHXTI>
2016-09-24 00:17:35 +0200

    Code fix. Forgotten variable update.

--- ex35.py
+++ ex35.py
@@ -33,6 +33,7 @@ def bear_room():
             dead("The bear looks at you then slaps your face off.")
         elif next == "taunt bear" and not bear_moved:
             print("The bear has moved from the door. You can go through it now.")
+            bear_moved = True
         elif next == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
         elif next == "open door" and bear_moved:
